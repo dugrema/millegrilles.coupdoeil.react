9c4ce894c463aaa2e175d5dc739955b1886d1e02
Mathieu Dugre <mathieu.dugre@mdugre.info>
2024-10-31 12:31:28 -0400

    Update filehost.

--- src/filehosting/FileHostingEdit.tsx
+++ src/filehosting/FileHostingEdit.tsx
@@ -1,9 +1,12 @@
+import { ChangeEvent, useCallback, useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
+import { ToggleSwitch } from "flowbite-react";
+
 import ActionButton from "../components/ActionButton";
 import useConnectionStore from "../connectionStore";
 import useWorkers from "../workers/workers";
-import { ChangeEvent, useCallback, useEffect, useState } from "react";
-import useFilehostStore from "./filehostingStore";
+import useFilehostStore, { FilehostStoreItem } from "./filehostingStore";
+import { FileHost } from "../workers/connection.worker";
 
 function FileHostingAdd() {
 
@@ -16,26 +19,32 @@ function FileHostingAdd() {
     let [error, setError] = useState('');
     let [urlExternal, setUrlExternal] = useState('');
     let [urlInternal, setUrlInternal] = useState('');
-    let urlExternalOnChange = useCallback((e: ChangeEvent<HTMLInputElement>)=>setUrlExternal(e.currentTarget.value), []);
-    let urlInternalOnChange = useCallback((e: ChangeEvent<HTMLInputElement>)=>setUrlInternal(e.currentTarget.value), []);
+    let urlExternalOnChange = useCallback((e: ChangeEvent<HTMLInputElement>)=>setUrlExternal(e.currentTarget.value), [setUrlExternal]);
+    let urlInternalOnChange = useCallback((e: ChangeEvent<HTMLInputElement>)=>setUrlInternal(e.currentTarget.value), [setUrlInternal]);
+    let [syncActive, setSyncActive] = useState(true);
+    let syncActiveOnChange = useCallback((checked: boolean)=>setSyncActive(checked), [setSyncActive]);
 
     let saveHandler = useCallback(async ()=>{
         if(!ready || !workers) throw new Error('workers not initialized');
-        let urlExternalParsed = new URL(urlExternal);
-        let urlInternalParsed = new URL(urlInternal);
-        console.debug("Adding file host with url external: %s, internal: %s", urlExternalParsed.href, urlInternalParsed.href);
+        let urlExternalParsed = urlExternal?new URL(urlExternal):null;
+        let urlInternalParsed = urlInternal?new URL(urlInternal):null;
+        console.debug("Adding file host with url external: %s, internal: %s", urlExternalParsed, urlInternalParsed);
 
         let fileHost = {
+            filehost_id: filehostId,
+            url_external: urlExternalParsed?urlExternalParsed.href:null,
+            url_internal: urlInternalParsed?urlInternalParsed.href:null,
+            sync_active: syncActive,
+        } as FileHost;
 
-        };
+        let response = await workers.connection.updateFileHost(fileHost);
+        console.debug("Response", response);
+        if(response.ok !== true) {
+            throw new Error('Error adding file host: ' + response.err);
+        }
 
-        // let response = await workers.connection.updateFileHost();
-        // console.debug("Response", response);
-        // if(response.ok !== true) {
-        //     throw new Error('Error adding file host: ' + response.err);
-        // }
         navigate('/coupdoeil2/fileHosting');
-    }, [workers, ready, filehostId, navigate, urlExternal, urlInternal]);
+    }, [workers, ready, filehostId, navigate, urlExternal, urlInternal, syncActive]);
 
     let testUrlHandler = useCallback(async ()=>{
     }, []);
@@ -60,8 +69,12 @@ function FileHostingAdd() {
             return  // No locking, gives a chance to reload the list
         };
 
+        setUrlExternal(filehost.url_external?filehost.url_external:'');
+        setUrlInternal(filehost.url_internal?filehost.url_internal:'');
+        setSyncActive(!!filehost.sync_active);
+
         setLocked(true);  // Prevent external change while editing
-    }, [ready, locked, filehostId, filehosts, setLocked, setError]);
+    }, [ready, locked, filehostId, filehosts, setLocked, setError, setUrlExternal, setUrlInternal, setSyncActive]);
 
     return (
         <>
@@ -74,9 +87,14 @@ function FileHostingAdd() {
 
             <section>
                 <div className='grid grid-cols-1 lg:grid-cols-12'>
-                    <label>Url (external)</label>
-                    <input placeholder="E.g.: https://myhost.com" value={urlExternal} onChange={urlExternalOnChange}
-                        className='col-span-11 text-black'/>
+                    <label htmlFor='urlInternalId' className='lg:col-span-2'>Url (external)</label>
+                    <input id='urlInternalId' placeholder="E.g.: https://myhost.com" value={urlExternal} onChange={urlExternalOnChange}
+                        className='col-span-10 text-black'/>
+                    <label htmlFor='urlExternalId' className='lg:col-span-2'>Url (internal)</label>
+                    <input id='urlExternalId' placeholder="E.g.: https://myhost.com" value={urlInternal} onChange={urlInternalOnChange}
+                        className='col-span-10 text-black'/>
+                    <label htmlFor='syncActiveId' className='lg:col-span-2'>Synchronisation active</label>
+                    <ToggleSwitch id='syncActiveId' checked={syncActive} onChange={syncActiveOnChange} className='grid-cols-10 pt-1 pb-1'/>
                 </div>
                 <div className='text-center pt-4'>
                     <ActionButton onClick={saveHandler} disabled={!ready||!workers} mainButton={true}>Save</ActionButton>
