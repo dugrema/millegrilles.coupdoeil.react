955ca93ba70b0600156d774965ddc5c712546aa0
Mathieu Dugre <mathieu.dugre@mdugre.info>
2026-07-24 15:49:03 -0400

    Removed last seen section from instance detail
    Now in card format

--- src/instances/InstanceDetail.tsx
+++ src/instances/InstanceDetail.tsx
@@ -19,37 +19,32 @@ function InstanceDetail() {
     }, [instances, instanceId]) as ManagerStatusV2 | null;
 
     return (
-        <>
+<div className="p-4 space-y-4">
             <Link to='/coupdoeil2/instances'
                 className='inline-flex items-center justify-center px-4 py-2 bg-slate-800 border border-slate-700 text-slate-200 hover:bg-slate-700 hover:scale-105 active:bg-slate-700 shadow-lg rounded-xl transition-all duration-200'>
                     Back
             </Link>
             <h1 className='text-xl font-bold pt-4'>Server instance {instance?.system_state?.host?.hostname}</h1>
 
-            <section className='grid grid-cols-2 pt-2 pb-4'>
-                <p>Last presence</p>
-                {instance?.timestamp}
-                {/* <ConditionalFormatters.FormatterConditionalDate value={instance?.timestamp} warn={60} error={600} /> */}
-            </section>
-
             <nav>
                 <Link to={`/coupdoeil2/instances/${instanceId}`}
                     className='inline-flex items-center justify-center px-4 py-2 bg-indigo-800 border border-indigo-700 text-white hover:bg-indigo-700 hover:scale-105 active:bg-indigo-700 shadow-lg rounded-xl transition-all duration-200'>
-                        Information
+                    Information
                 </Link>
                 <Link to={`/coupdoeil2/instances/${instanceId}/passwords`}
                     className='inline-flex items-center justify-center px-4 py-2 bg-slate-800 border border-slate-700 text-slate-200 hover:bg-slate-700 hover:scale-105 active:bg-slate-700 shadow-lg rounded-xl transition-all duration-200'>
-                        Passwords
+                    Passwords
                 </Link>
-<Link to={`/coupdoeil2/instances/${instanceId}/configure`}
+                <Link to={`/coupdoeil2/instances/${instanceId}/configure`}
                     className='inline-flex items-center justify-center px-4 py-2 bg-slate-800 border border-slate-700 text-slate-200 hover:bg-slate-700 hover:scale-105 active:bg-slate-700 shadow-lg rounded-xl transition-all duration-200'>
-                        Configure
-                    </Link>
+                    Configure
+                </Link>
             </nav>
 
 
             <Outlet context={{instance}} />
-        </>
+        </div>
+
     )
 }
 
